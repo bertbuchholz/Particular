f3d41d0c9f0f39e07d491312f71da04b6dec22c8
bertbuchholz <bert.b.buchholz@gmail.com>
2013-12-01 23:43:35 -0500

    Forgot files

---
+++ PolygonalCurve.cpp
@@ -0,0 +1,197 @@
+#include "PolygonalCurve.h"
+
+
+int Polygonal_curve::add_vertex_at_back(const Polygonal_curve::Point &vertex)
+{
+    _vertices.push_back(vertex);
+    return _vertices.size() - 1;
+}
+
+
+int Polygonal_curve::addVertexAtFront(const Polygonal_curve::Point &vertex)
+{
+    _vertices.insert(_vertices.begin(), vertex);
+    return _vertices.size() - 1;
+}
+
+
+void Polygonal_curve::insert_vertex(const int offset, const Polygonal_curve::Point &vertex)
+{
+    _vertices.insert(_vertices.begin() + offset, vertex);
+}
+
+
+Polygonal_curve::Point Polygonal_curve::get_pos_on_curve(const float s) const
+{
+    // std::cout << "PolygonalCurve::getPosOnCurve(): " << _lengthsAtPoints.size() << " " << s << std::endl;
+    assert(s >= 0.0f && s <= 1.0f);
+
+    if (s < 0.0001f) return _vertices.front();
+
+//    unsigned int i = 0;
+//    while (i < _vertices.size() - 1 && get_uniform_length_at_vertex(i) <= s)
+//    {
+//        ++i;
+//    }
+
+    int i = std::lower_bound(_lengths_at_points.begin(), _lengths_at_points.end(), _length * s) - _lengths_at_points.begin();
+
+    if (i >= _vertices.size() - 1) return _vertices.back();
+
+    assert(i < _vertices.size());
+
+    float s0 = get_uniform_length_at_vertex(i - 1);
+    float s1 = get_uniform_length_at_vertex(i);
+
+    assert(s >= s0 && s <= s1);
+
+    float offsetFromS1 = (s1 - s) / (s1 - s0);
+
+    return (_vertices[i - 1] * offsetFromS1 + _vertices[i] * (1.0f - offsetFromS1));
+}
+
+
+void Polygonal_curve::get_vertex_and_offset_at_length(float s, int &vertexIndex, float &offset) const
+{
+    // std::cout << "PolygonalCurve::getPosOnCurve(): " << _lengthsAtPoints.size() << " " << s << std::endl;
+    assert(s >= 0.0f && s <= 1.0f);
+
+    vertexIndex = 0;
+    offset = 0.0f;
+
+    if (s < 0.0001f) return;
+
+    while (vertexIndex < int(_vertices.size()) - 1 && get_uniform_length_at_vertex(vertexIndex) <= s)
+    {
+        ++vertexIndex;
+    }
+
+    if (vertexIndex >= int(_vertices.size()) - 1)
+    {
+        vertexIndex = _vertices.size() - 1;
+        return;
+    }
+
+    assert(vertexIndex < int(_vertices.size()));
+
+    float s0 = get_uniform_length_at_vertex(vertexIndex - 1);
+    float s1 = get_uniform_length_at_vertex(vertexIndex);
+
+    assert(s >= s0 && s <= s1);
+
+    // offset as seen from s0
+    offset = 1.0f - ((s1 - s) / (s1 - s0));
+}
+
+
+int Polygonal_curve::getClosestVertexIndexOnCurve(float s) const
+{
+    // std::cout << "PolygonalCurve::getPosOnCurve(): " << _lengthsAtPoints.size() << " " << s << std::endl;
+    assert(s >= 0.0f && s <= 1.0f);
+
+    if (s < 0.0001f) return 0;
+
+    unsigned int i = 0;
+    while (i < _vertices.size() - 1 && get_uniform_length_at_vertex(i) <= s)
+    {
+        ++i;
+    }
+
+    if (i >= _vertices.size() - 1) return _vertices.size() - 1;
+
+    assert(i < _vertices.size());
+
+    float s0 = get_uniform_length_at_vertex(i - 1);
+    float s1 = get_uniform_length_at_vertex(i);
+
+    assert(s >= s0 && s <= s1);
+
+    float offsetFromS1 = (s1 - s) / (s1 - s0);
+
+    if (offsetFromS1 > 0.5f) return (i - 1);
+
+    return i;
+
+    // return (vertices.at(i - 1).getPos() * offsetFromS1 + vertices.at(i).getPos() * (1.0f - offsetFromS1));
+}
+
+Polygonal_curve::Point Polygonal_curve::compute_tangent(const float s) const
+{
+    int index;
+    float offset;
+
+    get_vertex_and_offset_at_length(s, index, offset);
+
+    if (index == 0)
+    {
+        return (_vertices[1] - _vertices[0]).normalized();
+    }
+
+    return (_vertices[index] - _vertices[index - 1]).normalized();
+}
+
+
+void Polygonal_curve::finish()
+{
+    calcLength();
+}
+
+
+void Polygonal_curve::calcLength()
+{
+    _lengths_at_points.clear();
+
+    _length = 0.0f;
+
+    _lengths_at_points.push_back(0.0f);
+
+    for (unsigned int i = 1; i < _vertices.size(); ++i)
+    {
+        _length += (_vertices[i] - _vertices[i - 1]).norm();
+
+        _lengths_at_points.push_back(_length);
+    }
+
+    assert(_lengths_at_points.size() == _vertices.size());
+}
+
+
+float Polygonal_curve::get_length() const
+{
+    return _length;
+}
+
+
+float Polygonal_curve::get_uniform_length_at_vertex(const int index) const
+{
+    return _lengths_at_points[index] / _length;
+}
+
+
+float Polygonal_curve::get_absolute_length_at_vertex(const int index) const
+{
+    return _lengths_at_points[index];
+}
+
+
+float Polygonal_curve::distance_between_vertices(int index1, int index2) const
+{
+    float l1 = _lengths_at_points[index1];
+    float l2 = _lengths_at_points[index2];
+
+    if (l1 > l2)
+    {
+        std::swap(l1, l2);
+    }
+
+    float d1 = l2 - l1;
+
+    return d1;
+}
+
+
+void Polygonal_curve::clear()
+{
+    _vertices.clear();
+    _lengths_at_points.clear();
+}
---
+++ PolygonalCurve.h
@@ -0,0 +1,57 @@
+#ifndef POLYGONALCURVE_H
+#define POLYGONALCURVE_H
+
+#include <vector>\
+
+#include <Eigen/Core>
+
+class Polygonal_curve
+{
+public:
+    typedef Eigen::Vector3f Point;
+
+    int add_vertex_at_back(const Point& vertex);
+
+    int addVertexAtFront(const Point& vertex);
+
+    void insert_vertex(int const offset, const Point& vertex);
+
+    // return the 3d point on the curve at uniform curve length s
+    Point get_pos_on_curve(float const s) const;
+
+    // return the point on the curve at uniform curve length s
+    void get_vertex_and_offset_at_length(float s, int & vertexIndex, float & offset) const;
+
+    // return the point on the curve at uniform curve length s
+    int getClosestVertexIndexOnCurve(float s) const;
+
+    Point compute_tangent(float const s) const;
+
+    void finish();
+
+    void calcLength();
+
+    float get_length() const;
+
+    float get_uniform_length_at_vertex(int const index) const;
+
+    float get_absolute_length_at_vertex(int const index) const;
+
+    float distance_between_vertices(int index1, int index2) const;
+
+    std::vector<Point> const& get_vertices() const { return _vertices; }
+
+    std::vector<Point> & get_vertices() { return _vertices; }
+
+    void clear();
+
+private:
+    std::vector<Point> _vertices;
+
+    std::vector<float> _lengths_at_points;
+
+    float _length;
+};
+
+
+#endif // POLYGONALCURVE_H
